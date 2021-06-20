74adcfa02866871e7d3672fcfdd29d09b490f5a5
Thuvarakan <thuvamit2017@gmail.com>
2021-06-21 02:01:26 +0530

    change 1

--- add_product.php
+++ add_product.php
@@ -14,8 +14,11 @@
 <html>
 	<head>
 		<title>PowerGym</title>
-       <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">-->
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	   <link rel="stylesheet" type="text/css" href="css/style.css">
+	   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<?php include"navbar.php";?><br>
@@ -167,6 +170,7 @@
 					
 				</form>
 				</div>
+				
 				<div class="tbox" style="margin-bottom: 70px;">
 					<h3 style="margin-top:30px;"> Product Details</h3><br>
 					<?php
@@ -176,21 +180,23 @@
 						}
 					
 					?>
-                    <div id="table">
-					<table border="1px" >
-						
-                            <tr>
-                                <th>S.No</th>
-                                <th>product name</th>
-                                <th>Price</th>
-                                <th>Image</th>
-                                <th>Description</th>
-                                <th>Quantity</th>
-                                <th>Edit</th>
-                                <th>Delete</th>
-                            </tr>
-                        
-				 	<?php
+                   
+				   <div style="height: 500px; overflow-y: scroll; overflow-x: hidden;">
+                  <table class="table  table-hover table-bordered table-fixed">
+                    <thead class="thead-light">
+                      <tr>
+                                <th scope="col">S.No</th>
+                                <th scope="col">product name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Image</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Quantity</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody >
+					<?php
 							$s="select * from product";
 							$res=$db->query($s);
 							if($res->num_rows>0)
@@ -201,14 +207,14 @@
 									$i++;
 									echo "
 										<tr>
-											<td>{$i}</td>
-											<td>{$r["Product_Name"]}</td>
-											<td>{$r["Price"]}</td>
-											<td>{$r["Image"]}</td>
-											<td>{$r["Product_Description"]}</td>
-                                            <td>{$r["Quantity"]}</td>
-                                            <td><a href='add_product.php?id={$r["Product_ID"]}'  class='btnr'>Edit</a></td>
-											<td><a href='product_delete.php?id={$r["Product_ID"]}' class='btnr'>Delete</a></td>
+											<td scope='col'>{$i}</td>
+											<td scope='col'>{$r["Product_Name"]}</td>
+											<td scope='col'>{$r["Price"]}</td>
+											<td scope='col'>{$r["Image"]}</td>
+											<td scope='col'>{$r["Product_Description"]}</td>
+                                            <td scope='col'>{$r["Quantity"]}</td>
+                                            <td scope='col'><a href='add_product.php?id={$r["Product_ID"]}'  class='btnr'>Edit</a></td>
+											<td scope='col'><a href='product_delete.php?id={$r["Product_ID"]}' class='btnr'>Delete</a></td>
 										</tr>
 										";
 									
@@ -216,11 +222,12 @@
 								
 							}
 						?>
-					
-					</table>
-                        </div>
+                    </tbody>
+                  </table>
 				</div>
+                </div>
 			</div>
+		</div>
 	
 				<?php include"footer.php";?>
 	</body>
